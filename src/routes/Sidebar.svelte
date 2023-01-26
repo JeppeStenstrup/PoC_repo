9922068ae836072e8d90f28ea6bb530339e1057d
Jeppe Stenstrup Lauridsen <jepstrup@gmail.com>
2023-01-26 22:56:45 +0100

    Added submodules and more

---
+++ src/routes/Sidebar.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { fs } from 'fs';
+
+  fs.readdir('./notexist', function(err, files) {
+    if(err) { console.log(err); }
+
+    
+  });
+</script>
+
+<nav role="navbar">
+  <ul>
+    <li>1</li>
+    <li>2</li>
+  </ul>
+</nav>
+
+<style>
+  nav {
+    display: block;
+    margin: 0;
+    width: 250px;
+    background: blue;
+  }
+
+  ul {
+    display: block;
+    height: calc(100vh - 40px);
+    margin: 0;
+  }
+</style>
